8f2c64a4fae6c7ec95453ecfe99dafde83aac04c
Jorge Rivera <jorge.rivera@ssde.com.mx>
2023-09-16 22:34:54 -0600

    iOS-Dicee-00. Initial upload to personal repository

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,36 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
+/**
+ * ViewController.swift
+ * Dicee-iOS13
+ *
+ * Created by Angela Yu on 11/06/2019.
+ * Copyright © 2019 London App Brewery. All rights reserved.
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
+    // Linking UI elements in the view withh the view controller
+    @IBOutlet weak var imageViewDiceLeft: UIImageView!
+    @IBOutlet weak var imageViewDiceRight: UIImageView!
+    
+   
+    // Link an element from UI with an action
+    @IBAction func actionRollDicePressed(_ sender: UIButton) {
+        let diceValues = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")
+        ]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        /**
+         * Could it be also diceValues.randomElement() this chooses randomly one of the array elements,
+         * we'll use one method for each dice
+         */
+        imageViewDiceLeft.image = diceValues [Int.random(in: 0...5)]
+        imageViewDiceRight.image = diceValues.randomElement()
     }
-
-
+    
 }
-
